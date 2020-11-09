363ca248d47980ec2e06c8a2c30707575f9a1567
Hussein Mohamed <58296855+whos4n3@users.noreply.github.com>
2020-11-09 15:06:40 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Terraform state will be stored in S3
 terraform {
   backend "s3" {
-    bucket = "terraform-bucket-abdul"
+    bucket = "terraform-bucket-hussein"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
